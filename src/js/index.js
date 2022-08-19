e343a6d2a1b350c6543661c0fe532bee1891ea4d
telnova.anastasia@gmail.com <telnova.anastasia@gmail.com>
2022-08-19 16:00:10 +0300

    edit adaptive

--- src/js/index.js
+++ src/js/index.js
@@ -2,8 +2,18 @@ const swiper = new Swiper('.slider__outer-wrap', {
 
 	direction: 'horizontal',
 	loop: true,
-	slidesPerView: 2,
+	slidesPerView: 1.15,
 	spaceBetween: 37,
+	breakpoints: {
+
+		991: {
+			slidesPerView: 2,
+		},
+
+		320: {
+			spaceBetween: 10,
+		}
+	},
 
 	navigation: {
 		nextEl: '.slider__button-next',
@@ -12,4 +22,21 @@ const swiper = new Swiper('.slider__outer-wrap', {
 
 });
 
-// console.log('working');
+const burger = document.querySelector('.header__burger-lines')
+const menu = document.querySelector('.burger')
+const overlay = document.querySelector('.overlay')
+const cross = menu.querySelector('.burger__cross')
+
+burger.addEventListener('click', openMenu)
+
+function openMenu(event) {
+	menu.classList.add('active')
+	overlay.classList.add('active')
+	cross.addEventListener('click', closeMenu)
+	overlay.addEventListener('click', closeMenu)
+}
+
+function closeMenu() {
+	menu.classList.remove('active')
+	overlay.classList.remove('active')
+}
